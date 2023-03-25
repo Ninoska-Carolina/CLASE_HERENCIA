1622b4c777fe8ce5b11a4459060c97ad4d291ba7
CAROLINA <caro@gmail.com>
2023-03-25 00:35:32 -0400

    modificaiones del proyecto

---
+++ CLASES HERENCIA/ESTUDIANTES_SIS-INF.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLASES_HERENCIA
+{
+    internal class ESTUDIANTES_SIS_INF:Usuario
+    {
+        public decimal Costo_Matricula { get; set; }
+        public override string ToString()
+        {
+
+            return $"CUENTA: {Cuenta} | Pass:{Password} | Nombre:{Nombre} | Matricula:{Matricula} | Costo_Matricula:{Costo_Matricula} ";
+             
+        }
+    }
+}
